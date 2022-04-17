e5c44d7e9effb4f1170b4302e69c969ba2eb56f5
wwj31 <229482191@qq.com>
2022-04-17 21:08:20 +0800

    no message

--- timer/timer_test.go
+++ timer/timer_test.go
@@ -34,15 +34,35 @@ func TestErrTimer1(t *testing.T) {
 
 // 永久等待timer.C例子2
 func TestErrTimer2(t *testing.T) {
+	println(time.Now().Unix())
 	ti := time.NewTimer(time.Second)
 	time.Sleep(2 * time.Second)
 
-	ti.Reset(10 * time.Second)
+	ti.Reset(1 * time.Second)
+	time.Sleep(2 * time.Second)
 
-	<-ti.C
-	println("c1")
-	<-ti.C
-	println("c2")
+	tt := <-ti.C
+	println("c1", tt.Unix())
+	tt = <-ti.C
+	println("c2", tt.Unix())
 	// 直接调用reset导致里面的chan没有消费干净，所以第一次能直接取出c1
 	// 而c2需要等到reset的10秒才会触发,从而导致非预期结果
+}
+
+// 永久等待timer.C例子2
+func TestErrTimer3(t *testing.T) {
+	ti := time.NewTimer(time.Second)
+
+	go func() {
+		ti.Stop()
+	}()
+	time.Sleep(1 * time.Second)
+
+	if !ti.Stop() {
+		println("<-C")
+		<-ti.C
+		println("bbb")
+	}
+	ti.Reset(1 * time.Second)
+	println("ok")
 }
